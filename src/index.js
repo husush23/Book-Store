6a86d0c8e95d29f9bc9c492e0f10e60715d559c7
husush23 <husseinkadare40@gmail.com>
2023-06-23 13:59:27 +0300

    update added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-/*eslint-disable*/
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
 import App from './App';
@@ -12,5 +11,5 @@ root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
